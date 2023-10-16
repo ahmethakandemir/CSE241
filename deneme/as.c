21955a4e5225695f842f669bd497846bbc8f544d
ahmethakandemir <72705464+ahmethakandemir@users.noreply.github.com>
2023-10-16 12:27:24 +0300

    deneme added

---
+++ deneme/as.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("deneme");
+    return 0;
+}
---
+++ deneme/new.cpp
@@ -0,0 +1,19 @@
+// @ts-ignore
+#include <cstring>
+#include <iostream>
+
+
+int main() {
+    using namespace std;  // Use the std namespace
+
+
+    int *p = new int;
+    *p = 3;
+
+    cout << *p << endl;
+
+
+
+
+    return 0;
+}
