e287333b9f2e0515630679eb0a58c2d60565b246
ahmethakandemir <hakandemir1625@icloud.com>
2024-01-09 18:31:48 +0300

    .

--- hw2/mainn.cpp
+++ hw2/mainn.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <list>
 #include <string>
+#include <cstring>
+#include <fstream>
 
 using namespace std;
 
@@ -16,7 +18,10 @@ public:
     virtual void rmdir() = 0;
     void mkdir(const string &name);
     void cd(const string &name);
-    virtual void printToFile() const = 0;
+    virtual void printToFile(ofstream &filename) const = 0;
+    void getFromFile(ifstream &filename);
+    void cp(const string &pathname);
+    virtual ~MyShell() = default;
 
 protected:
     Directory *currentDirectory;
@@ -30,6 +35,7 @@ public:
     File(const string &name) : name(name) {}
     string name;
     string content;
+    Directory *parent;
     virtual void display() const override
     {
         cout << "File: " << name << endl;
@@ -46,11 +52,13 @@ public:
     {
         return content.size();
     }
-    virtual void printToFile() const override
-    {
-        cout << "[~[" << name << "~~";
-        cout << "~~" << content << "]~]";
-    }
+    void printToFile(ofstream &filename) const override;
+private:
+    string pwd() const override {return "";}
+    void ls() const override{}
+    void rmdir() override{}
+    void rm(const string &name) override{}
+
 
 };
 
@@ -68,40 +76,172 @@ public:
     void ls() const override;
     void rmdir() override;
     void rm(const string &name) override;
-    void display() const override
-    {
-        
-        for (const auto &directory : directories)
-        {
-            cout << endl;
-            directory->pwd();
-            directory->display();
-        }
-        for (const auto &file : files)
-        {
-            file->display();
-            cout << ", ";
-        }
-    }
-    virtual void printToFile() const override
-    {
+    void display() const override;
+    void printToFile(ofstream &filename) const override;
+    
 
-        for (const auto &directory : directories)
-        {
-            directory->printToFile();
-        }
-        for (const auto &file : files)
-        {
-            file->printToFile();
-        }
 
-    }
+
 };
 
-void MyShell::printToFile() const
+void MyShell::cp(const string &pathname){
+    try{
+        ifstream filename(pathname);
+        if(!filename.is_open()){
+            throw 1;
+        }
+        string textContent;
+        string line;
+        while(getline(filename, line)){
+            textContent += line;
+        }
+        filename.close();
+        File* newfile = new File(pathname);
+        newfile->parent = currentDirectory;
+        newfile->changeContent(textContent);
+        currentDirectory->files.push_back(newfile);
+    }
+    catch(...){
+        cout << "cp: cannot stat '" << pathname << "': No such file or directory" << endl;
+        return;
+    }
+    
+    
+    
+}
+
+void MyShell::getFromFile(ifstream &filename)
 {
-    root->display();
-    cout << endl;
+    currentDirectory = nullptr;
+    size = 0;
+    root = new Directory(nullptr, "root");
+    currentDirectory = root;
+
+    string line;
+    while (getline(filename, line))
+    {
+        if (line == "" || line == "\n" || line[0] != '/')
+        {
+            continue;
+        }
+
+        int i = 0;
+        while (i < line.size())
+        {
+            string name;
+            while (line[i] != '/' && i < line.size() && line[i] != ' ')
+            {
+                name += line[i];
+                i++;
+            }
+            if(line == "/root\n"){
+                continue;
+            }
+            if (name == "root")
+            {
+                currentDirectory = root;
+            }
+            else
+            {
+                bool isin = false;
+                for (const auto &directory : currentDirectory->directories)
+                {
+                    if (directory->name == name)
+                    {
+                        currentDirectory = directory;
+                        isin = true;
+                        break;
+                    }
+                }
+                if (!isin)
+                {
+                    if(name != "root"){
+                        mkdir(name);
+                        cd(name);
+                    }
+                }
+            }
+    
+            // cout << line << endl;
+            // cout << line[i] << int(line[i]) << line[i + 1] << int(line[i + 1]) << endl;
+            if(line[i] == ' ')
+                i++;
+            if(line[i] == '['){
+                if(line[i + 1] == '~'){
+                    // cout << "here" ;
+                    if(line[i + 2] == '['){
+                        string filesname;
+                        i += 3;
+                        while(!((line[i] == '~') && (line[i + 1] == '~') && (line[i + 2] == '~') && (line[i + 3] == '~'))){
+                            filesname += line[i];
+                            i++;
+                        }
+                        i += 4;
+                        string content;
+                        while(!((line[i] == ']') && (line[i + 1] == '~') && (line[i + 2] == ']'))){
+                            content += line[i];
+                            i++;
+                        }
+                        i += 3;
+                        File* newfile = new File(filesname);
+                        newfile->parent = currentDirectory;
+                        newfile->changeContent(content);
+                        currentDirectory->files.push_back(newfile);
+                    }
+                }
+            }
+            
+
+            if (line[i] == '/')
+            {
+                i++;
+            }
+        }
+
+        currentDirectory = root;
+    }
+}
+
+void MyShell::printToFile(ofstream &filename) const
+{
+    root->printToFile(filename);
+}
+
+void Directory::printToFile(ofstream &filename) const
+{
+    filename << "\n" << pwd();    
+    for (const auto &directory : directories)
+    {
+        if(directory->name != "")
+            directory->printToFile(filename);
+    }
+    for (const auto &file : files)
+    {
+        filename << endl << "/" << name;
+        file->printToFile(filename);
+    }
+}
+
+
+void File::printToFile(ofstream &filename) const
+{
+    filename << parent->pwd();
+    filename << " [~[" << name << "~~";
+    filename << "~~" << content << "]~]" << endl;
+}
+
+void Directory::display() const{
+    for (const auto &directory : directories)
+    {
+        cout << endl;
+        directory->pwd();
+        directory->display();
+    }
+    for (const auto &file : files)
+    {
+        file->display();
+        cout << ", ";
+    }
 }
 
 void Directory::rm(const string &name)
@@ -126,6 +266,10 @@ void Directory::rmdir()
     {
         if ((*iterator)->name == name)
         {
+            if(((*iterator)->directories.size() != 0) || ((*iterator)->files.size() != 0)){
+                cout << "rmdir: failed to remove '"<< name << "': Directory not empty" << endl;
+                return;
+            }
             directories.erase(iterator);
             return;
         }
@@ -137,11 +281,12 @@ void Directory::ls() const
 {
     for (const auto &directory : directories)
     {
-        cout << directory->name << endl;
+        if(directory->name != "")
+            cout << "D " << directory->name << endl;
     }
     for (const auto &file : files)
     {
-        cout << file->name;
+        cout << "F " << file->name << endl;
     }
 }
 
@@ -150,13 +295,17 @@ string Directory::pwd() const
     string path;
     if (parent == nullptr)
     {
-        cout << name;
+        path = path + "/" + name;
     }
     else
     {
-        parent->pwd();
-        cout << '/' << name;
-        path = path + '/' + name;
+        Directory tempdir = *this;
+        while (tempdir.parent != nullptr)
+        {
+            path = "/" + tempdir.name + path;
+            tempdir = *tempdir.parent;
+        }
+        path = "/" + tempdir.name + path;
     }
     return path;
 }
@@ -195,11 +344,19 @@ void Directory::start()
     size = 0;
     root = new Directory(nullptr, "root");
     currentDirectory = root;
+    try{
+        ifstream filename("db.txt");   
+        getFromFile(filename);
+    }
+    catch(...){
+        cout << "db.txt not found" << endl;
+    }
+
     string command;
     while (true)
     {
         cout << "> ";
-        currentDirectory->pwd();
+        cout << currentDirectory->pwd();
         cout << "$ ";
         cin >> command;
         if (command == "mkdir")
@@ -221,7 +378,7 @@ void Directory::start()
         }
         else if (command == "pwd")
         {
-            currentDirectory->pwd();
+            cout << currentDirectory->pwd();
             cout << endl;
         }
         else if (command == "ls")
@@ -240,15 +397,50 @@ void Directory::start()
         }
         else if (command == "print")
         {
-            printToFile();
+            string file = "db.txt";
+            ofstream filename(file);
+            
+            root->printToFile(filename);
+        }
+        else if(command == "cp"){
+            string pathname;
+            cin >> pathname;
+            cp(pathname);
+        }
+        else if (command == "cat")
+        {
+            string name;
+            cin >> name;
+            for (const auto &file : currentDirectory->files)
+            {
+                if (file->name == name)
+                {
+                    file->displayContent();
+                    break;
+                }
+            }
+        }
+        else if (command == "display")
+        {
+            currentDirectory->display();
+        }
+        else if (command == "clear")
+        {
+            system("clear");
+        }
+        
+        else{
+            cout << "invalid command" << endl;
         }
     }
 }
 
 int main()
 {
+    
     Directory *root = new Directory();
     root->start();
 
     return 0;
 }
+
